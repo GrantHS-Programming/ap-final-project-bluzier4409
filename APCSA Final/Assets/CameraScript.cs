4d41aeee23f63e710cef1b3825da0bb4c114518a
bluzier4409 <bluzier4409@student.pps.net>
2024-05-09 10:02:39 -0700

    May 9th Commit

--- APCSA Final/Assets/CameraScript.cs
+++ APCSA Final/Assets/CameraScript.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 20f;
+    private Vector3 motion;
+    private Rigidbody rb;
+    
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        mouseMove();
+    }
+
+    void mouseMove()
+    {
+        float h = 2f * Input.GetAxis("Mouse X");
+        float v = 2f * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(v, h, 0);
     }
 }
