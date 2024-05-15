c070b925ca2a5d21a3c446ea76fd0686c09cb383
bluzier4409 <bluzier4409@student.pps.net>
2024-05-15 10:01:24 -0700

    May 15th Commit
    Created directional movement. Working on jumping, friction, and other movement options.

--- APCSA Final/Assets/Movement.cs
+++ APCSA Final/Assets/Movement.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using Unity.VisualScripting;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Vector3 blah = new Vector3(0,0,0);
+    Vector3 blah = new Vector3(50,50,50);
     Rigidbody rb;
+    public LayerMask ground;
+    public float playerHeight;
+
+    public int groundDrag = 7;
+
+    public float maxVel = 20f;
+
+    public GameObject empty;
+
+    public Camera c;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +29,56 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        jump();
+        wKey();
+        aKey();
+        sKey();
+        dKey();
+    }
+
+    void wKey() 
+    {
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            rb.AddForce(c.transform.forward * 3, ForceMode.Force);
+        }
+    }
+
+    void jump() 
+    {
+        print(grounder());
+        if (Input.GetKeyDown(KeyCode.Space) && grounder()) 
         {
             rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
         }
+    }
 
-        if (Input.GetKeyDown(KeyCode.W)) 
+    void dKey() 
+    {
+        if (Input.GetKey(KeyCode.D)) 
         {
-            rb.AddForce(rb.transform.forward * 300, ForceMode.Force);
+            rb.AddForce(c.transform.right * 3, ForceMode.Force);
         }
+    }
+
+    void sKey()
+    {
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            rb.AddForce(c.transform.forward * -3, ForceMode.Force);
+        }
+    }
+
+    void aKey() 
+    {
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            rb.AddForce(c.transform.right * -3, ForceMode.Force);
+        }
+    }
+
+    bool grounder()
+    {
+        return Physics.Raycast(empty.transform.position, Vector3.down, playerHeight, ground);
     }
 }
