a624964cc9b04ecf87c2917d88ae05fccb04d3bd
bluzier4409 <bluzier4409@student.pps.net>
2024-05-23 09:57:24 -0700

    May  23rd Work
    Worked more on grappling hook

--- APCSA Final/Assets/GrapplingGun.cs
+++ APCSA Final/Assets/GrapplingGun.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,6 +12,12 @@ public class GrapplingGun : MonoBehaviour
 
   public LayerMask grappleable;
 
+  public float maxDist = 50f;
+
+  private SpringJoint joint;
+
+  public Transform GunTip, CameraHolder, Player;
+
   void Awake()
   {
     lr = GetComponent<LineRenderer>();
@@ -19,12 +27,56 @@ public class GrapplingGun : MonoBehaviour
   {
     if (Input.GetMouseButtonDown(0))
     {
-       // Grapple();
+       grapple();
     }
     else if (Input.GetMouseButtonUp(0))
     {
-      //  StopGrapple();
+      stopGrapple();
     }
+  }
+
+  void LateUpdate()
+  {
+      draw();
+  }
+
+  void grapple()
+  {
+    RaycastHit hit;
+    Physics.Raycast(CameraHolder.position, CameraHolder.forward, out hit, maxDist, grappleable);
+
+    GrapplePoint = hit.point;
+    joint = Player.gameObject.AddComponent<SpringJoint>();
+    joint.autoConfigureConnectedAnchor = false;
+    joint.connectedAnchor = GrapplePoint;
+
+    float distanceFromPoint = Vector3.Distance(Player.position, GrapplePoint);
+
+    //Distance to keep player in
+
+    joint.maxDistance = distanceFromPoint * 0.8f;
+    joint.minDistance = distanceFromPoint * 0.6f;
+
+    // Spring Variables
+    joint.spring = 6f;
+    joint.damper = 3f;
+    joint.massScale = 5f;
+
+    lr.positionCount = 2;
+  }
+
+  void draw()
+  {
+    if (!joint) return;
+    lr.SetPosition(0, GunTip.position);
+    lr.SetPosition(1, GrapplePoint);
+
+  }
+
+  void stopGrapple()
+  {
+    lr.positionCount = 0;
+    Destroy(joint);
   }
 
 }
